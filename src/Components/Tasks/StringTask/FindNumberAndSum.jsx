0239f4ac640e5b0c2d6a844c59f7370881ede1d5
MherKK <Mherkevorkian2@gmail.com>
2023-11-24 23:42:56 +0400

    fixed my grammer in string task

--- src/Components/Tasks/StringTask/FindNumberAndSum.jsx
+++ src/Components/Tasks/StringTask/FindNumberAndSum.jsx
@@ -5,7 +5,7 @@ export default function FindNumberAndSum(){
     const [sum,setSum] = useState();
     return (
         <div style={{border:'1px solid black' , padding:'20px'}}>
-            <h2>Pick up only the numbers from a string and find there sum</h2>
+            <h2>Pick up only the numbers from a string and find their sum</h2>
             <input type="text" placeholder="write a random text" style={{height:'30px',width:'180px'}} onChange={(e) => setText(e.target.value)}/>
             <button  onClick={() => {
                 let textArray = text.split('');
