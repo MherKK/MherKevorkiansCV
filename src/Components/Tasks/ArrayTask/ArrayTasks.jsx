fa8eea350f8d10d35f33d94588c7495f3752950e
MherKK <Mherkevorkian2@gmail.com>
2023-11-26 16:46:31 +0400

    added filter and sort array task

--- src/Components/Tasks/ArrayTask/ArrayTasks.jsx
+++ src/Components/Tasks/ArrayTask/ArrayTasks.jsx
@@ -1,10 +1,12 @@
 import BinarySearch from "./BnarySearch";
+import FilterSort from "./FilterSort";
 import ReverseArray from "./ReverseArray";
 
 
 export default function ArrayTasks(){
     return (<div   style={{display:'flex' , flexDirection:'column', alignItems:'center',justifyContent:'center', gap:'20px', width:'350px',marginBottom:'100px'}}>
         <BinarySearch />
+        <FilterSort />
         <ReverseArray />
     </div>)
 }
---
+++ src/Components/Tasks/ArrayTask/FilterSort.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+
+
+
+export default function FilterSort(){
+
+    let array = [1, 4, 5,6, 2, 8, 12, 15, 17, 19];
+    let [filterArray, setFilterArray] = useState(array);
+    let [finalArray, setFinalArray] = useState(filterArray);
+
+    useEffect(() => {
+        setFinalArray(filterArray)
+    },[finalArray,filterArray])
+    
+    function arraySorter(array) {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = i; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    let swap = array[i];
+                    array[i] = array[j];
+                    array[j] = swap
+                    //[array[i], array[j]] = [array[j], array[i]]
+                }
+            }
+        }
+        return array;
+    }
+  
+    return (
+        <div  style={{border:'1px solid black' , padding:'20px'}}>
+            <h2>Filter and sort the array with the following options</h2>
+            <div style={{marginBottom:'20px'}}>Filter By:
+                <select onClick={(e) => {
+                    if(e.target.value === 'all'){
+                        setFilterArray(array)
+                    }else if(e.target.value === 'even'){
+                        setFilterArray(array.filter(num => num % 2 === 0))
+                    }else if(e.target.value === 'odd'){
+                        setFilterArray(array.filter(num => num % 2 !== 0))
+                    }else if(e.target.value === 'num>10'){
+                        setFilterArray(array.filter(num => num > 10))
+                    }else if(e.target.value === 'num<10'){
+                        setFilterArray(array.filter(num => num < 10))
+                    }
+                }}> 
+                    <option value='all'>All Numbers</option>
+                    <option value="even">Even Numbers</option>
+                    <option value="odd">Odd Numbers</option>
+                    <option value="num>10">{`Numbers > 10`}</option>
+                    <option value="num<10">{`Numbers < 10`}</option>
+                </select>
+            </div>
+            
+            <div>Sort By:
+                <select onChange={(e) => {
+                    if(e.target.value === 'asc'){
+                        setFilterArray(arraySorter(array))
+                    }else if(e.target.value === 'desc') {
+                        setFilterArray(array.sort((a, b) => b - a))
+                    }
+                }}> 
+                    <option>Choose order</option>
+                    <option value="asc">Ascending Order</option>
+                    <option value="desc">Descending Order</option>
+                </select>
+            </div>
+
+            <p  style={{textAlign:'left'}}>Original Array: [{array.map((key,index) => {
+                if(index !== array.length - 1) {
+                    return key + ','
+                }else{
+                    return key
+                }
+            })}]</p>
+            <p  style={{textAlign:'left'}}>Result:[ {finalArray.map((key,index) => {
+                if(index !== finalArray.length - 1) {
+                    return key + ','
+                }else{
+                    return key
+                }
+            })} ] </p>
+        </div>
+    )
+}
